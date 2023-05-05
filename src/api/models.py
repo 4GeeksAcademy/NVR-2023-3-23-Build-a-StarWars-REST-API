f4df8438241324428c3a3b7b0637a7ec1b15b57b
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-05-05 12:09:58 +0000

    Initial Models for user, favourite, character, location and episode tabnles

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favourites = db.relationship("Favourite", backref="user")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +18,29 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favourite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category = db.Column(db.Enum('characters', 'locations', 'episodes'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    episode_id = db.Column(db.Integer, db.ForeignKey('episode.id'))
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    species = db.Column(db.String(80), nullable=False)
+    favourites = db.relationship("Favourite", backref="character")
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    type = db.Column(db.String(80))
+    favourites = db.relationship("Favourite", backref="location")
+
+class Episode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    air_date = db.Column(db.String(10))
+    favourites = db.relationship("Favourite", backref="episode")
